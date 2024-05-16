bb9163c258f70a17f7212ae41d4a627fe0159577
satoshi-yoshiji <satoshi.yoshiji@mail.mcgill.ca>
2024-05-16 01:30:59 -0400

    latest

--- GWAS_pipeline/gwas_impute37_regenie/11a-gwas-s2-imp37-qc-filter_female.sh
+++ GWAS_pipeline/gwas_impute37_regenie/11a-gwas-s2-imp37-qc-filter_female.sh
@@ -38,7 +38,8 @@
 imp_file_dir="/Bulk/Imputation/UKB imputation from genotype/"
 #set this to the exome data field for your release
 project="UKB"
-data_field="ukb22418"
+#data_field="ukb22418"
+data_field="ukb22828" # NOTE THE ID IS DIFFERENT FOR THE IMPUTATION DATA
 # working dir and reference text file dir
 data_file_dir="/03.sex_stratified_BMI/gt_genrel_block/"
 txt_file_dir="/03.sex_stratified_BMI/phenotype_covariate/"
@@ -51,7 +52,7 @@ for i in {1..22}; do
       --make-pgen --out ukbi_ch${i}_v3; \
     plink2 --pfile ukbi_ch${i}_v3 \
       --no-pheno --keep ${phenotype_file} \
-      --maf 0.006 --mac 20 --geno 0.1 --mind 0.1 \
+	  --mac 50 --geno 0.1 --mind 0.1 \
       --make-bed --out ${data_field}_c${i}_v3; \
      rm ukbi_ch${i}_v3* "
 
@@ -68,9 +69,9 @@ done
       --sample ${data_field}_cX_b0_v3.sample \
       --make-pgen --out ukbi_chX_v3; \
     plink2 --pfile ukbi_chX_v3 \
-      --no-pheno --keep  \
-      --maf 0.006 --mac 20 --geno 0.1 --mind 0.1 \
-      r-make-bed --out ${data_field}_cX_v3; \
+      --no-pheno --keep ${phenotype_file}  \
+	  --mac 50 --geno 0.1 --mind 0.1 \
+      -make-bed --out ${data_field}_cX_v3; \
      rm ukbi_chX_v3* "
     
     dx run swiss-army-knife -iin="${imp_file_dir}/${data_field}_cX_b0_v3.bgen" \
